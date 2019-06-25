d104c9cd60b0228c04467a56014d1829c8f9c829
Rybczinski <rybczinski0726@gmail.com>
2019-06-25 19:58:48 +0900

    Inner Beans, Aliases and idref

--- src/main/java/org/ryb/maven/Drawing.java
+++ src/main/java/org/ryb/maven/Drawing.java
@@ -3,17 +3,13 @@ package org.ryb.maven;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Hello world!
- *
- */
 public class Drawing {
     public static void main(String[] args) {
         // Triangle triangle = new Triangle();
         // BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
-        Triangle triangle = (Triangle) context.getBean("triangle");
+        Triangle triangle = (Triangle) context.getBean("triangle-alias");
         triangle.draw();
     }
 }
